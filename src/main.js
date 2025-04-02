fa2f43e05e84fb7c3bf1c456016f6f16f97e154f
Felipe Sabogal <csabogal@grupodot.com>
2025-04-01 22:01:22 -0500

    Historial de navegacion y pagina de tendencias

--- src/main.js
+++ src/main.js
@@ -87,3 +87,24 @@ async function getMoviesByCateogry(id) {
 
   createMovies(movies,genericSection);
 }
+
+async function getMoviesBySearch(query) {
+  const { data } = await api("search/movie", {
+    params: {
+      query,
+    },
+  });
+
+  const movies = data.results;
+
+  createMovies(movies,genericSection);
+}
+
+async function getTrendingMovies() {
+  const { data } = await api(`trending/movie/day`);
+
+  const movies = data.results;
+
+  createMovies(movies, genericSection);
+}
+
